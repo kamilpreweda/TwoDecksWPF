1187c0918fa81986702ef90c3b52408ef7306bea
Kamil Preweda <preweda.kamil@o2.pl>
2022-06-08 12:38:42 +0200

    Added 'Reset' method in 'Deck' class

--- TwoDecksWPF/Deck.cs
+++ TwoDecksWPF/Deck.cs
@@ -9,6 +9,19 @@ namespace TwoDecksWPF
 {
     internal class Deck : ObservableCollection<Card>
     {
+        private static Random random = new Random();
 
+        public Deck()
+        {
+            Reset();
+        }
+
+        public void Reset()
+        {
+            Clear();
+            for (int suit = 0; suit <= 3; suit++)
+                for (int value = 1; value <= 13; value++)
+                    Add(new Card((Values)value, (Suits)suit));
+        }
     }
 }
